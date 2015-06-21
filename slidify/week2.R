33205148bf62bf82c10422726b92dc3b8f30a3ea
kumar-velugula <velugula2003@yahoo.com>
2015-06-21 19:24:35 -0400

    Data products slidify commit

---
+++ slidify/week2.R
@@ -0,0 +1,9 @@
+install.packages("devtools")
+library(devtools)
+install_github('slidify', 'ramnathv')
+ install_github('slidifyLibraries','ramnathv')
+library(slidify)
+
+setwd("~/sample/project")
+
+author("first_deck")
